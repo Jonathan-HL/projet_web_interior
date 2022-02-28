74ea62509293dcd50ba56b75aa70a174a556a7a6
Jonathan <jonxplod@outlook.fr>
2022-02-28 16:08:49 +0100

    nav edit

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -11,22 +11,27 @@
               <li><a href={{ route("projects") }}>Projects</a></li>
               <li><a href={{ route("blog") }}>Blog</a></li>
               <li><a href={{ route("contact") }}>Contact</a></li>
-            </ul>
-            {{-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                @auth
-                <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+              @if (Route::has('login'))
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-        </div> --}}
+                @auth
+                <li>
+
+                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                </li>
+                @else
+                <li>
+
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                </li>
+
+                @if (Route::has('register'))
+                <li>
+                    <a href="{{ route('register') }}" class=" text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                </li>
+                @endif
+                @endauth
+                @endif
+            </ul>
           </nav><!-- #nav-menu-container -->
         </div>
     </div>
